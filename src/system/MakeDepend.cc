3f289580a4c6fe23613dd436ca570fb1a39ed1e0
Neil Weisenfeld <neil@10xgenomics.com>
2015-05-19 19:19:46 -0700

    major MakeDepend change - put everything into libCRD.a
    - now everything other than executable .cc's will end up in libCRD.a
    - old behavior was only modules referenced by an executable in the
      repository would be included.
    - this is to facilitate inclusion of modules that will only
      be used by code external to the repository.

--- src/system/MakeDepend.cc
+++ src/system/MakeDepend.cc
@@ -743,6 +743,14 @@ void generateMakefile( ostream& os, SourceMap const& db )
                 generateExeBits(os,db,file);
                 DepsSet const& deps = file.getLinkerDependents(db);
                 libObjects.insert(deps.begin(),deps.end());
+            } else {
+            	// WARNING!!!!
+            	// this here hack is so that EVERYTHING that's not an executable
+            	// gets tossed into the libCRD.a
+            	//
+            	// that's to facilitate the inclusion of code that will ONLY
+            	// be used externally -- a big change from previous behavior
+            	libObjects.insert(&file);
             }
         }
     }
